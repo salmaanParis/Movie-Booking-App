1621a6afab877f39a0a569ed57af74c7b4a779cf
salmaanParis <youtonez@gmail.com>
2024-09-26 22:20:38 +0530

    url modified

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http:movie-booking-app-delta.vercel.app/api',
+  baseURL: 'https://movie-booking-app-taupe.vercel.app/api', 
   headers: {
     'Content-Type': 'application/json',
   },
@@ -11,6 +11,3 @@ const api = axios.create({
 
 export default api;
 
-//baseURL: 'http:movie-booking-app-delta.vercel.app',
-
-//baseURL: 'http://localhost:3000/api',
